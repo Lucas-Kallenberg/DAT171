01b185db4087c6fdb053f95faf0912a893705d64
lucaskallenberg <lucaskallenberg@gmail.com>
2022-02-19 14:14:10 +0100

    'Hej'

--- Lab 1/CA1.py
+++ Lab 1/CA1.py
@@ -11,6 +11,14 @@ from matplotlib.collections import LineCollection
 from scipy.spatial import cKDTree
 
 def read_coordinate_file(filename):  # Create a function that reads the txtfile and rename it as file
+    """ Reads a textfile and converts the numbers into coordinates
+
+    :param filename: Textfile containing coordinate points that has '{' and '}' around them
+    :type filename: txt file
+
+    :return: Array of coordinates
+    :rtype: Numpy array of arrays of floats
+    """
 
     with open(filename, 'r') as file:
         lst =[]  # Create an empty list to store my arrays
@@ -28,38 +36,59 @@ def read_coordinate_file(filename):  # Create a function that reads the txtfile 
 
     return arr
 
-def plot_points(coord, indices): # path
-    fig = plt.figure()  # Create a figure
+def plot_points(coord, indices,path):
+    """ Plots the 'coords' as points and then drawing lines between chosen 'indices'
+    and then drawing the shortest path of two points on the lines
+
+    :param coord: Coordinates that sould be ploted
+    :type: Array of arrays
+    :param indices: Specific points of which coords that lines should be between
+    :type: Arrays of arrays
+    :param path: The shortest path between two predefined points on top of the lines
+    :type: Array of int
+
+    :return: Shows a plot
+    :rtype: Have no type
+    """
+    fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)   # Create a subplot in the figure to be able to add stuff later
     plt.scatter(coord[:,0],coord[:,1], s = 2, c= 'r')  # Plot the coords as nodes
     seg = np.empty((len(indices),2,2))
 
     for i,p in enumerate(indices): #Take each array in indices
-        print(p)
-        seg[i,:,0] = coord[p[0]][0] - coord[p[1]][0]  #x
-        seg[i,:,1] = coord[p[0]][1] - coord[p[1]][1] #y
-    print(seg)
-
-
+        seg[i,:,0] = [coord[p[0]][0], coord[p[1]][0]]   #x
+        seg[i,:,1] = [coord[p[0]][1],coord[p[1]][1]]   #y
     ax.add_collection(LineCollection(seg,color= 'gray',linewidths= 0.2))    # Drawing the lines to the plot
 
-
+    seg = np.empty((len(path), 2, 2))
     for i in range(len(path)-1):
-        # Here we draw the shortest path by drawing lines between the nodes by taking x0,y0 and x1,y1 of the paths nodes
-        lines = LineCollection([[(coord[path[i]][0],coord[path[i]][1]), (coord[path[i+1]][0],coord[path[i+1]][1])]],color= 'blue',linewidths= 1)
-        ax.add_collection(lines)    # Drawing the lines to the plot
+        # Segments between points
+        seg[i,:,0] = [coord[path[i]][0],coord[path[i+1]][0]]
+        seg[i,:,1] = [coord[path[i]][1],coord[path[i+1]][1]]
+    ax.add_collection(LineCollection(seg,color= 'blue',linewidths= 1))    # Drawing the lines to the plot
 
-    plt.autoscale
+
+    plt.gca().set_aspect('equal')
     plt.show()
 
 def construct_graph_connections(coord_list, radius):
+    """ Creates two lists, one with the distances between coordinates that are within a certain radius
+    and a list of the numbers of the coordinates
+
+    :param coord_list: Checking if the coordinats ar within a certain radius of each other and storying those values
+    :type: Array of arrays
+    :param radius: Predefined value that the function checks against
+    :type: Float
+
+    :return: Distances between coords within the radius. Also returns which number of the coords the distance is between
+    :rtype: 'distance' list of floats, 'coords' list of arrays
+    """
     distance = []
     coords = []
-    for n, a in enumerate(coord_list):  # We create a number infront of the arrays and then we loop over the arrays
-        for i in range(n+1, len(coord_list)):  # Now we take the second array of the coord_list
+    for n, a in enumerate(coord_list):  # Create a number infront of the arrays and then we loop over the arrays (For me to understand)
+        for i in range(n+1, len(coord_list)):  # Takes the second array of the coord_list
             dist = np.linalg.norm(a - coord_list[i])  # Calculates the distance using pythagoras of the coords
-            dist = np.array(dist)
-            if dist <= radius:  # Checking if the coords distance is inside of the radius that is set
+            if dist <= radius:  #Checks if the coords are within a certain radius
                 distance.append(dist)
                 coords.append([n,i])
 
@@ -67,12 +96,40 @@ def construct_graph_connections(coord_list, radius):
     coords = np.array(coords)
     return distance , coords
 
-def construct_graph(distance, indices,N):     # N is the length of the distances
+def construct_graph(distance, indices,N):
+    """ Creates a sparse matrix
+
+    :param distance: The distance between the coordinates of the indices.
+    :type: List of floats
+    :param indices: The numbers of the coordinates that the distances are between
+    :type: A list of arrays
+    :param N: A predefined int that is the length of the list distance
+    :type: int
+
+    :return: A sparse matrix with the numbers of the coordinates and distances between them
+    :rtype: csr_matrix
+    """
     indices = indices.T     # Transpose to get the right input
     s = sp.sparse.csr_matrix((distance, indices), shape=[N,N])   # Saves the numbers between the points and the distance in a sparce matrix
     return s
 
 def find_shortest_path(graph, start_node, end_node):
+    """
+    nodes, shortdist = find_shortest_path(sparsegrahp, start, end)
+
+
+    :param graph: The sparse matrix that the function checks the values of
+    :type: csr matrix
+    :param start_node: A predefined value of which coordinate number to start at
+    :type: int
+    :param end_node: A predefined value of which coordinate number to end at
+    :type: int
+
+    :return: 'list' A list of the shortest path of the nodes that it goes through
+    'shortestdistance' is the sum of distances between all the connecting nodes in the list from start to end
+    :rtype: 'list' is array of ints, 'shortestdistance' is a float
+    """
+
     dist, path = csgraph.shortest_path(graph, directed= False, return_predecessors= True, indices= (start_node))
     # dist = the shortest distance from each node from the starting node, path = last node before
     shortesdistance = dist[end_node]    # We take the distance from the end node
@@ -82,29 +139,41 @@ def find_shortest_path(graph, start_node, end_node):
     # This while loop will be checking if we are back at the start node by starting at our last node and working its
     # way back by looking at the last nodes predecessor and so on
     while last > 0:
-        list.append(path[last])     # We add the preceding node of the last node
+        list.append(path[last])     # Add the preceding node of the last node
         last = path[last]
         if last == -9999:   # If there is no path the value of list/last will be -9999
-            del list[-1]    # We delete the last value of the list
+            del list[-1]    # Delete the last value of the list
+
 
     list.reverse()   # Get the order from start to end node
     list = np.array(list)
     return list, shortesdistance
 
 def construct_fast_graph_connections(coord_list, radius):
+    """Creates two lists, one with the distances between coordinates that are within a certain radius
+    and a list of the numbers of the coordinates
 
-    tree = sp.spatial.cKDTree(coord_list)       # We create a tree
-    dist = []    # Create some lists
+    :param coord_list: Checking if the coordinats ar within a certain radius of each other and storying those values
+    :type: Array of arrays
+    :param radius: Predefined value that the function checks against
+    :type: Float
+
+    :return: 'dist' distances between coords within the radius. 'indices' returns which number of the coords the distance is between
+    :rtype: 'dist' list of floats, 'indices' list of arrays
+    """
+    tree = sp.spatial.cKDTree(coord_list)
+    dist = []
     indices = []
     points = tree.query_ball_point(coord_list, r=radius)     # Creates lists with the nearby nodes withing range of the choosen coordinate
 
-    for i, point in enumerate(points):      # We check if the element is larger than i to make it undirected
+    for i, point in enumerate(points):      # Check if the element is larger than i to make it undirected
         for j in point:
             if j >= i:
                 indices.append(np.array([i, j]))
 
-    for i, j in indices:   # Here we get our distance
-        dist.append(np.linalg.norm(coord_list[i]-coord_list[j]))
+    speed = np.array(indices)
+    up = np.array([coord_list[speed[:, 0]], coord_list[speed[:,1]]])
+    dist = np.linalg.norm(up[0]-up[1], axis=-1)
 
     indices = np.array(indices)   # Convert
     dist = np.array(dist)
@@ -115,7 +184,7 @@ def construct_fast_graph_connections(coord_list, radius):
 if __name__=='__main__':
 
     # Choose file to read: 1 = Sample, 2 = Hungary, 3 = Germany
-    Choose = 1
+    Choose = 2
 
     # Choose fast or slow graph conections: True or False
     Fast = False
@@ -165,23 +234,21 @@ if __name__=='__main__':
     else:
         print('Write in true or false')
 
-  #  N = len(distance)
-
-   # start_time = time.time()
-    #sparsegrahp = construct_graph(distance, coords,N)
-   # end_time = time.time()
-    #print('construct_graph \t\t\t\t time: ', end_time - start_time)
-
-   # start_time = time.time()
-    #nodes, shortdist = find_shortest_path(sparsegrahp, start, end)
-   # end_time = time.time()
-   # print('find_shortest_path \t\t\t\t time: ', end_time - start_time)
-
-    print('coord',arr)
-    print('indices',coords)
+    N = len(distance)
 
     start_time = time.time()
-    plot_points(arr, coords) #nodes
+    sparsegrahp = construct_graph(distance, coords,N)
+    end_time = time.time()
+    print('construct_graph \t\t\t\t time: ', end_time - start_time)
+
+    start_time = time.time()
+    nodes, shortdist = find_shortest_path(sparsegrahp, start, end)
+    end_time = time.time()
+    print('find_shortest_path \t\t\t\t time: ', end_time - start_time)
+
+
+    start_time = time.time()
+    plot_points(arr, coords,nodes)
     end_time = time.time()
     print('plot_points \t\t\t\t\t time: ', end_time - start_time)
 
